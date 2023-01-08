9117d83cf88ee9724353dd93c64128a6435a4e51
Desmarchelier Maxime <maxime.desmarchelier@ensiie.fr>
2023-01-08 13:02:31 +0100

    LevelController now generate passengerlist

--- Assets/Script/LevelEasyController.cs
+++ Assets/Script/LevelEasyController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Script;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -14,14 +15,15 @@ public class LevelEasyController : MonoBehaviour
     private void Awake(){
         GameController.Instance.Score = 0;
         GameController.Instance.Level = "LEVEL-EASY";
-        GameController.Instance.NbPassengerRemaining = 5;
-        GameController.Instance.NbFraudster = 3;
+        GameController.Instance.NbPassengerRemaining = 10;
+        GameController.Instance.NbFraudster = 5;
 
         var train = Instantiate(trainPrefab, new Vector3(-22.95f, 1.064f, 3.86f), Quaternion.identity);
         train.SetActive(true);
         var trainController = train.GetComponent<TrainController>();
         trainController.MoveToStation(new Vector3(5f, 1.064f, 3.86f));
-        trainController.PassengerSpawnList = new List<Vector3>
+
+        trainController.PassengerList = GeneratePassengerList(new List<Vector3>
         {
             new(12.87f, 0, -7.63f),
             new(6.08f, 0, -7.63f),
@@ -29,10 +31,26 @@ public class LevelEasyController : MonoBehaviour
             new(-3.603f, 0, -7.63f),
             new(-6.491f, 0, -7.63f),
             new(-13.292f, 0, -7.63f)
-        };
+        }, Quaternion.identity, GameController.Instance.NbPassengerRemaining, GameController.Instance.NbFraudster);
 
         trainController.ExitPosition = new Vector3(44f, 1.064f, 3.86f);
         _trains.Add(trainController);
+    }
+
+    private List<GameObject> GeneratePassengerList(List<Vector3> spawnPositionList, Quaternion passengerRotation,
+        int nbPassenger, int nbFraudster){
+        var passengerList = new List<GameObject>();
+
+        for (int i = 0; i < nbPassenger; i++)
+        {
+            int randomIndex = Random.Range(0, spawnPositionList.Count);
+            var passenger = Instantiate(passengerPrefab, spawnPositionList[randomIndex], passengerRotation);
+            var passengerController = passenger.GetComponent<PassengerController>();
+            passengerController.Fraudster = true;
+            passengerList.Add(passenger);
+        }
+
+        return passengerList;
     }
 
     // Start is called before the first frame update
@@ -54,7 +72,7 @@ public class LevelEasyController : MonoBehaviour
             train.State = "LEFT";
         }
 
-        if (_trains.Any(train => train.PassengerCount != 0)) return;
+        Debug.Log(GameController.Instance.NbPassengerRemaining);
 
         if (GameController.Instance.NbPassengerRemaining == 0) SceneManager.LoadScene("Scoreboard");
     }
--- Assets/Script/TrainController.cs
+++ Assets/Script/TrainController.cs
@@ -1,70 +1,60 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
-public class TrainController : MonoBehaviour
+namespace Script
 {
-    public string State { get; set; }
-    public List<Vector3> PassengerSpawnList { get; set; }
+    public class TrainController : MonoBehaviour
+    {
+        public string State { get; set; }
+        public List<GameObject> PassengerList { get; set; }
+        public Vector3 ExitPosition { get; set; }
 
-    public List<GameObject> PassengerList { get; set; }
-
-    public Vector3 ExitPosition { get; set; }
-    public int PassengerCount { get; set; }
-
-    // Start is called before the first frame update
-    private void Start(){
-    }
-
-    // Update is called once per frame
-    private void Update(){
-    }
-
-    private void Awake(){
-    }
-
-    private IEnumerator MoveCoroutine(Vector3 stationPosition, bool destroy){
-        State = "MOVING";
-        while (Vector3.Distance(transform.position, stationPosition) > 0.1f)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, stationPosition, 0.01f);
-            yield return null;
+        private void Awake(){
         }
 
-        if (destroy)
-        {
-            Destroy(gameObject);
-        }
-        else
-            State = "STATION";
-    }
-
-    public void MoveToStation(Vector3 stationPosition){
-        StartCoroutine(MoveCoroutine(stationPosition, false));
-    }
-
-    private IEnumerator SpawnPassenger(GameObject passengerPrefab, int number){
-        while (number > 0)
-        {
-            number--;
-            yield return new WaitForSeconds(2f);
-            // Get random element from PassengerSpawnList
-            var spawnPosition = PassengerSpawnList[Random.Range(0, PassengerSpawnList.Count)];
-            var passenger = Instantiate(passengerPrefab, spawnPosition, Quaternion.identity);
-
-            passenger.SetActive(true);
+        // Start is called before the first frame update
+        private void Start(){
         }
 
-        State = "UNLOADED";
-    }
+        // Update is called once per frame
+        private void Update(){
+        }
 
-    public void UnloadPassengers(GameObject passengerPrefab, int passengerNumber){
-        StartCoroutine(SpawnPassenger(passengerPrefab, passengerNumber));
-    }
+        private IEnumerator MoveCoroutine(Vector3 stationPosition, bool destroy){
+            State = "MOVING";
+            while (Vector3.Distance(transform.position, stationPosition) > 0.1f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, stationPosition, 0.01f);
+                yield return null;
+            }
 
-    public void MoveAway(){
-        StartCoroutine(MoveCoroutine(ExitPosition, true));
+            if (destroy)
+                Destroy(gameObject);
+            else
+                State = "STATION";
+        }
+
+        public void MoveToStation(Vector3 stationPosition){
+            StartCoroutine(MoveCoroutine(stationPosition, false));
+        }
+
+        private IEnumerator SpawnPassenger(GameObject passengerPrefab, int nbPassengerRemaining, int fraudsterNumber){
+            foreach (var passenger in PassengerList)
+            {
+                passenger.SetActive(true);
+                yield return new WaitForSeconds(2f);
+            }
+
+            State = "UNLOADED";
+        }
+
+        public void UnloadPassengers(GameObject passengerPrefab, int nbPassengerRemaining, int fraudsterNumber){
+            StartCoroutine(SpawnPassenger(passengerPrefab, nbPassengerRemaining, fraudsterNumber));
+        }
+
+        public void MoveAway(){
+            StartCoroutine(MoveCoroutine(ExitPosition, true));
+        }
     }
 }
